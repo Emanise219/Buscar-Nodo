17b304aee6a3aa58f08da7f91a937cede8155288
Emanise219 <78283418+Emanise219@users.noreply.github.com>
2021-02-20 13:58:32 -0400

    Add files via upload

---
+++ Lista.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BUSCAR_NODO
+{
+    class Lista
+    {
+        private Nodo primero = new Nodo();
+        private Nodo ultimo = new Nodo();
+
+        public Lista()
+        {
+            primero = null;
+            ultimo = null;
+
+        }
+        public void buscarNodo()
+        {
+            Nodo actual = new Nodo();
+            actual = primero;
+            bool encontrado = false;
+            Console.WriteLine("Ingrese el dato del nodo a buscar:");
+            int nodoBuscar = int.Parse(Console.ReadLine());
+            while (actual !=null && encontrado == false)
+            {
+                if (actual.Dato == nodoBuscar)
+                {
+                    Console.WriteLine("\n Nodo con el dato ({0}) encontrado \n" + actual.Dato);
+                    encontrado = true;
+                }
+               
+                actual = actual.Siguiente;
+            }
+            if (!encontrado)
+            {
+                Console.WriteLine("\n Nodo no encontrado\n");
+            }
+
+        }
+        
+    }
+}
---
+++ Nodo.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BUSCAR_NODO
+{
+    class Nodo
+    {
+        private int dato;
+        private Nodo siguente;
+        private Nodo atras;
+
+
+        public int Dato
+        {
+            get { return dato; }
+            set { dato = value; }
+        }
+        public Nodo Siguiente
+        {
+            get {  return siguente; }
+            set { siguente = value; }
+
+        }
+        public Nodo Atras
+        {
+            get { return atras; }
+            set { atras = value; }
+        }
+            
+        
+    }
+}
